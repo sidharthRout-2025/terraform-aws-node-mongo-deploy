fb560fe211c02d2b8f698baa295c17e0c5323d5c
sidharthRout-2025 <sidharthrout7005@gmail.com>
2025-11-11 11:19:38 +0530

    Create main.tf

---
+++ terraform/modules/network/main.tf
@@ -0,0 +1,42 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+  tags = { Name = "${var.project_name}-vpc" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+  tags = { Name = "${var.project_name}-igw" }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+  vpc_id = aws_vpc.this.id
+  cidr_block = var.public_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags = { Name = "${var.project_name}-public-${count.index}" }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+  vpc_id = aws_vpc.this.id
+  cidr_block = var.private_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
+  tags = { Name = "${var.project_name}-private-${count.index}" }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "${var.project_name}-public-rt" }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count = length(aws_subnet.public)
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}

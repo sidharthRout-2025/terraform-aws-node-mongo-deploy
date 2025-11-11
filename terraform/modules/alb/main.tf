ffdac9c6c223c5545e94b4464d870dd7e3683888
sidharthRout-2025 <sidharthrout7005@gmail.com>
2025-11-11 11:11:00 +0530

    Create main.tf

---
+++ terraform/modules/alb/main.tf
@@ -0,0 +1,51 @@
+# Security group for ALB
+resource "aws_security_group" "alb_sg" {
+  name = "${var.project_name}-alb-sg"
+  vpc_id = var.vpc_id
+  description = "ALB security group - allow 80"
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = { Name = "${var.project_name}-alb-sg" }
+}
+
+resource "aws_lb" "alb" {
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = var.public_subnets
+  tags = { Name = "${var.project_name}-alb" }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "${var.project_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  health_check {
+    path = "/"
+    port = "80"
+    protocol = "HTTP"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}

dbb0941d0bb34b26867a73a33c6a151c7f2b4082
sidharthRout-2025 <sidharthrout7005@gmail.com>
2025-11-11 11:15:52 +0530

    Create iam.tf

---
+++ terraform/modules/ecs_ec2/iam.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "${var.project_name}-ecs-instance-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_instance_assume_role.json
+}
+
+data "aws_iam_policy_document" "ecs_instance_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_AmazonEC2ContainerServiceforEC2Role" {
+  role = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_read" {
+  role = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "${var.project_name}-ecs-instance-profile"
+  role = aws_iam_role.ecs_instance_role.name
+}

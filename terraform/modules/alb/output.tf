7a3e1c7ed4d9ecef3f0251bb2b021375f8ea3602
sidharthRout-2025 <sidharthrout7005@gmail.com>
2025-11-11 11:11:28 +0530

    Create output.tf

---
+++ terraform/modules/alb/output.tf
@@ -0,0 +1,4 @@
+output "alb_dns_name" { value = aws_lb.alb.dns_name }
+output "alb_arn" { value = aws_lb.alb.arn }
+output "alb_sg_id" { value = aws_security_group.alb_sg.id }
+output "target_group_arn" { value = aws_lb_target_group.tg.arn }
